4513ac1d60c5406494be2750f054961714292311
Carol Wang <carolzjwang@gmail.com>
2025-02-15 18:27:59 -0500

    modify testingSQL.py and app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
-from testingSQL import startup, get_map
+from testingSQL import startup, get_map, get_filters
 
 app = Flask(__name__)
 
@@ -15,14 +15,19 @@ locations = []
 
 @app.route("/")
 def home():
-    print("Hello World!")
-    return jsonify("Hello World!")
+    return jsonify(get_map)
 
-@app.route("/map")
-def map():
-    startup()
-    number = get_map()
-    return jsonify(number)
+@app.route("/filter", methods=['GET'])
+def filter():
+    if request.method == 'GET':
+        location = request.args["loc"]
+        date = request.args["date"]
+        start_time = request.args["start_time"]
+        end_time = request.args["end_time"]
+        searches = request.args["search"].split()
+    
+    events = get_filters(location, date, start_time, end_time, searches)
+    return jsonify(events)
 
 if __name__ == "__main__":
     app.run()
