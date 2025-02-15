ec581405411efc7326fccf0a6c55f2ba4c0fa9ff
2kbyte <evantran06@gmail.com>
2025-02-15 00:37:40 -0500

    commiting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import * as React from 'react';
+
+function ButtonUsage() {
+  return <Button variant="contained">Events</Button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body-wrapper">
+        <div className="img-wrapper">
+          <p>
+            Discover, Explore, Find.
+          </p>
+          <a/>
+          <ButtonUsage /> {/* Button component included here */}
+        </div>
+      </div> 
     </div>
   );
 }
---
+++ src/Events.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './Event.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
