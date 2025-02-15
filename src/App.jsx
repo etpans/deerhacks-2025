ff9d6781ee4ec92ee5bb3b225ba41c66131660c7
Carol Wang <carolzjwang@gmail.com>
2025-02-15 16:50:42 -0500

    modified testingSQL

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import LandingContent from './components/Landing';
-import Events from './components/Events';
+import { useState , useEffect } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
+import LandingContent from './components/Landing'
+import Events from './components/Events'
+import EventWidget from './components/EventWidget'
 import 'leaflet/dist/leaflet.css';
 import EventWidget from './components/EventWidget';
 
 function App() {
-    const [showEvents, setShowEvents] = useState(false);
+
+  const [showEvents, setShowEvents] = useState(false)
+  const [count, setCount] = useState("");
+
+  const fetchMap = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/map');
+      const itemData = await response.json();
+      setCount(itemData);
+    }
+    catch(error){console.log(error)}
+  }
+
+  useEffect(() => {
+    fetchMap();
+  }, []);
+
+  const handleClick = () => {
+    alert("Button clicked!");
+    fetchMap();
+    console.log(count);
+  };
 
     return (
         <>
